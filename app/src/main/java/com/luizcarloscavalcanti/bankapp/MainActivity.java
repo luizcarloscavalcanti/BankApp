14f39ddd78c0b82418f7d8d567098780625a51d3
LUIZ CAVALCANTI <luizcarlos.cavalcanti@outlook.com>
2021-01-07 23:34:29 -0300

    Criação da segunda tela, mostrando as informações do usuário após o login

--- app/src/main/java/com/luizcarloscavalcanti/bankapp/MainActivity.java
+++ app/src/main/java/com/luizcarloscavalcanti/bankapp/MainActivity.java
@@ -2,13 +2,28 @@ package com.luizcarloscavalcanti.bankapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText user, password;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        user = findViewById(R.id.editTextUser);
+        password = findViewById(R.id.editTextPassword);
+        btnLogin = findViewById(R.id.buttonLogin);
+    }
+
+    public void clickBotaoLogin(View view) {
+        startActivity(new Intent(MainActivity.this, Currency.class));
     }
 }
