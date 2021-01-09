c6011b24b176eacdd0fa2bf93509006cd18f640a
LUIZ CAVALCANTI <luizcarlos.cavalcanti@outlook.com>
2021-01-09 03:29:41 -0300

    Adicionado endpoint que retornará uma lista de lançamentos para o usuário

--- app/src/main/java/com/luizcarloscavalcanti/bankapp/service/Api.java
+++ app/src/main/java/com/luizcarloscavalcanti/bankapp/service/Api.java
@@ -1,11 +1,14 @@
 package com.luizcarloscavalcanti.bankapp.service;
 
 import com.luizcarloscavalcanti.bankapp.model.LoginResponse;
+import com.luizcarloscavalcanti.bankapp.model.StatementsResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface Api {
 
@@ -15,4 +18,10 @@ public interface Api {
             @Field("user") String user,
             @Field("password") String password
     );
+
+    @FormUrlEncoded
+    @GET("statements/{userId}")
+    Call<StatementsResponse> statements(
+            @Path("userId") int userId);
+
 }
