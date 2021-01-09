6feb87a81dcda2f0987460c1327df01c78297c02
LUIZ CAVALCANTI <luizcarlos.cavalcanti@outlook.com>
2021-01-09 03:27:58 -0300

    Modelo de Objeto, para pegar as informações do usuario logado

--- app/src/main/java/com/luizcarloscavalcanti/bankapp/model/UserAccount.java
+++ app/src/main/java/com/luizcarloscavalcanti/bankapp/model/UserAccount.java
@@ -2,19 +2,36 @@ package com.luizcarloscavalcanti.bankapp.model;
 
 public class UserAccount {
 
-    private String userId, name;
+    private String name;
+    private Integer userId;
+    private Float balance;
+    private Long agency, bankAccount;
 
-    public UserAccount(String userId, String name){
-        this.userId = userId;
+    public UserAccount(String name, Integer userId, Long bankAccount, Long agency, Float balance) {
         this.name = name;
-    }
-
-    public String getUserId() {
-        return userId;
+        this.userId = userId;
+        this.bankAccount = bankAccount;
+        this.agency = agency;
+        this.balance = balance;
     }
 
     public String getName() {
         return name;
     }
 
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public Long getBankAccount() {
+        return bankAccount;
+    }
+
+    public Long getAgency() {
+        return agency;
+    }
+
+    public Float getBalance() {
+        return balance;
+    }
 }
