f4993b8ae2254968350dcefc11c69132a0a02af8
LUIZ CAVALCANTI <luizcarlos.cavalcanti@outlook.com>
2021-01-10 21:04:43 -0300

    Criado o modelo StatementList e suas propriedades

---
+++ app/src/main/java/com/luizcarloscavalcanti/bankapp/model/StatementList.java
@@ -0,0 +1,37 @@
+package com.luizcarloscavalcanti.bankapp.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class StatementList {
+
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("desc")
+    @Expose
+    private String desc;
+    @SerializedName("date")
+    @Expose
+    private String date;
+    @SerializedName("value")
+    @Expose
+    private Double value;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+}
